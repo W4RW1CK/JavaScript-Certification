7215def61bb345cf46c05902faae7602c850b3f0
W4RW1CK <ejcv0403@protonmail.com>
2026-01-20 05:10:13 -0600

    Build a JavaScript Trivia Bot

---
+++ Variables and Strings/lab-javascript-trivia-bot.js
@@ -0,0 +1,18 @@
+// start of script.js
+
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "W4rw1ck";
+const botLocation = "the world" ;
+const favoriteLanguage = "Rust";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "codingFact of " + favoriteLanguage;
+console.log(codingFact);
+codingFact = "secondCodingFact of " + favoriteLanguage;
+console.log(codingFact);
+codingFact = "thirdCodingFact of " + favoriteLanguage;
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+// end of script.js
+
