f28227f2863f723c1c8aedd43142143011bfd917
W4RW1CK <ejcv0403@protonmail.com>
2026-01-31 23:03:56 -0600

    Build a String Repeating Function

---
+++ Loops/build-a-string-repeating-function.js
@@ -0,0 +1,11 @@
+// start of script.js
+
+const repeatStringNumTimes = (string, number) => {
+  let a = "";
+  for (let i = 0; i < number; i++) {
+    a += string;
+  }
+  return a;
+}
+
+// end of script.js
