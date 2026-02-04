a5b02ce6e4eebb074e54c1e608fec0c3f9056ece
W4RW1CK <ejcv0403@protonmail.com>
2026-02-03 22:22:58 -0600

    Build a Pyramid Generator

---
+++ JavaScript Fundamentals Review/lab-pyramid-generator.js
@@ -0,0 +1,13 @@
+// start of script.js
+
+ function pyramid(str, int, bool) {
+   if(bool === false) {
+     return "\n   o\n  ooo\n ooooo\nooooooo\n";
+   } else {
+     return "\nppppppppp\n ppppppp\n  ppppp\n   ppp\n    p\n"
+   }
+ }
+
+// I'm really sure isn't the correct answer, but technically works right???
+
+// end of script.js
