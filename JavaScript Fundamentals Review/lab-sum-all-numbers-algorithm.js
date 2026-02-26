afb55089a3b9446c055ae0d46b2b416ecc6e9438
w4rw1ck <ejcv0403@protonmail.com>
2026-02-25 23:02:55 -0600

    Design a Sum All Numbers Algorithm

---
+++ JavaScript Fundamentals Review/lab-sum-all-numbers-algorithm.js
@@ -0,0 +1,15 @@
+// start of script.js
+
+const sumAll = (arr) => {
+  const min = Math.min(arr[0], arr[1]);
+  const max = Math.max(arr[0], arr[1])
+
+  let sum = 0;
+
+  for(let i = min; i <= max; i++) {
+      sum += i;
+    }
+  return sum
+}
+
+// end of script.js
